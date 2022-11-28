04b55203553ba008f04abae0ef745c7991490274
M1778M <m1778.pc@gmail.com>
2022-11-29 01:20:21 +0330

    Make clt importable and usable

--- OMEGAPy-Project/omg/clt.py
+++ OMEGAPy-Project/omg/clt.py
@@ -1,19 +1,47 @@
 # CommandLineTool
-from .tools import listTool
-from .xlang import xinter,xbase,methods
+from .tools import listTool as _listTool
+from .xlang import xinter as _xinter,xbase as _xbase,methods as _methods
 
-readline=methods.ReadLine()
+readline=_methods.ReadLine()
 
-_compiler = xbase.xi()
-_cltsyn = xbase.Syntax.from_dict({'Write':'stdout.write','Read':'stdin.read','ReadLine':'stdin.readline'})
-xinter.press_syntax(_cltsyn,_compiler)
+_compiler = _xbase.xi()
+_cltsyn = _xbase.Syntax.from_dict({'Write':'stdout.write','Read':'stdin.read','ReadLine':'stdin.readline'})
+_xinter.press_syntax(_cltsyn._syndict,_compiler)
 
 code = \
 """
-# SoruceIsClt yes
-# SourceIsValid yes
-# SourceWillCompile yes
+# HasCompile yes
+# IsOmgLibrary yes
+# HasMain no
+
 $PyRun clt_module
+import time
+from colorama import init as _init,Fore,Back
+from threading import Thread
+PyDef sprint(*args,fc='',bc=''):
+    for arg in args:
+        print(str(fc)+str(bc)+str(arg),end='')
+PyDef reset(w=2):
+    if w == 2: print(end=f'{Fore.RESET}{Back.RESET}')
+    if w == 1: print(end=f'{Fore.RESET}')
+    if w == 0: print(end=f'{Back.RESET}')
+global _flag
+_flag = 0
+PyDef waiting_flag():
+    global flag
+    flag=1
+
+PyDef Waiting ( func : type(reset) , args=(waiting_flag,) ):
+    global _flag
+    if _flag == 1:
+        _flag=0
+    prx = Thread(target=func , args=args)
+    prx.run()
+    while True:
+        if _flag == 1:
+            return True
+        sprint("Waiting...\\r")
+        time.sleep(0.5)
 
 
 $End clt_module
@@ -29,4 +57,8 @@ Clt Syntax:
 $End clt_comment
 """
 
-_compiler(code)
+_rdsyn=_compiler.xiread(code)
+_rdsyn=_rdsyn['clt_module']
+_gsyn = _compiler.xisyntax(_rdsyn)
+_compiler._execute(_gsyn)
+from .xlang.xbase import sprint,Waiting,waiting_flag,_flag,reset
